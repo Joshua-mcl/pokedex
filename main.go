9330028d2209809f0199f6b4d8da47a537b3d4b7
Joshua Mclaren <joshuamclarenprog24@gmail.com>
2025-03-02 08:48:19 -0700

    refactor repl into it's own file

--- main.go
+++ main.go
@@ -1,19 +1,8 @@
 package main
 
-import ("fmt"
-		"os"
-		"bufio"
-)
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Print(" >")
-
-	scanner.Scan()
-	text := scanner.Text()
-
-	fmt.Println("echoing: ", text)
+	startRepl()
 
 	
 }
